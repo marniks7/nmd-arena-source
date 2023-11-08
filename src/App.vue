5c13a300862b59e33b6a024a206facfd158b95aa
marniks7 <marniks7@gmail.com>
2023-11-07 23:07:56 -0500

    Add image gallery

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,8 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
@@ -57,21 +43,11 @@ nav a:first-of-type {
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
   .logo {
     margin: 0 2rem 0 0;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 
   nav {
     text-align: left;
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,88 +1,120 @@
-<script setup lang="ts">
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
-</script>
-
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
+  <div class="actions-container">
+    <button @click="clearCards">Clear</button>
+    <button @click="saveCards">Save</button>
+  </div>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
+  <div class="slot-container">
+    <div class="input-container">
+      <select v-model="selectedOption" id="select-option">
+        <option value="option1">Runner</option>
+        <option value="option2">Carry</option>
+        <option value="option3">Versatile (option1)</option>
+        <option value="option4">Versatile (option2)</option>
+        <option value="option5">Master of Fish</option>
+      </select>
+      <select v-model="selectedMapOption" id="select-map-option">
+        <option value="dark-prison">Dark Prison</option>
+        <option value="raven-nest">Raven Nest</option>
+        <option value="ablaze-cave">Ablaze Cave</option>
+      </select>
+    </div>
 
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
+    <div class="top-grid">
+      <div
+          class="card-slot"
+          v-for="slot in topGridSlots"
+          :key="slot.id"
+          @dragstart="startDrag(slot)"
+          @dragend="endDrag"
+          draggable="true"
+      >
+        <div class="static-text">{{ slot.text }}</div>
+      </div>
+    </div>
+  </div>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
+  <div class="bottom-grid" id="library">
+    <!-- Library cards will be added via Vue data -->
+    <!--      <div-->
+    <!--          class="card"-->
+    <!--          v-for="card in libraryCards"-->
+    <!--          :key="card.id"-->
+    <!--          draggable="true"-->
+    <!--          @dragstart="startDrag(card)"-->
+    <!--          @dragend="endDrag"-->
+    <!--      >-->
+    <!--        <img :src="card.image" :alt="card.text" />-->
+    <!--&lt;!&ndash;        {{ card.text }}&ndash;&gt;-->
+    <!--      </div>-->
+    <v-container>
+      <v-row>
+        <v-col
+            cols="2"
+            v-for="card in libraryCards"
+            :key="card.id"
+            class="gallery-image"
+        >
+          <v-img :src="card.image"/>
+        </v-col>
+      </v-row>
+    </v-container>
 
-    <br />
-
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedOption: 'option1',
+      selectedMapOption: 'dark-prison',
+      topGridSlots: [
+        {id: 1, text: ''},
+        {id: 2, text: ''},
+        {id: 3, text: '1st boss'},
+        {id: 4, text: '2nd boss'},
+        {id: 5, text: ''},
+      ],
+      libraryCards: [/* Add your library card objects here */],
+    };
+  },
+  methods: {
+    loadCardImages() {
+      const cardImages = Object.values(import.meta.glob('@/assets/cards/*.{png,jpg,jpeg,PNG,JPEG}', {
+        eager: true,
+        as: 'url'
+      }))
+      // const cardImages = require.context('@/assets/cards', false, /\.(jpg|jpeg|png|gif)$/);
+      cardImages.forEach((imagePath) => {
+        const image = new URL(imagePath, import.meta.url).href;
+        //const image = cardImages(imagePath);
+        // Extract the card name from the image file name (filename without extension)
+        const cardName = imagePath.split('/').pop().replace(/\.\w+$/, ''); // Extracts the filename without extension
+        this.libraryCards.push({
+          image,
+          text: cardName,
+        });
+      });
+    },
+
+    clearCards() {
+      // Logic to clear cards
+    },
+    saveCards() {
+      // Logic to save cards
+    },
+    startDrag(item) {
+      // Logic for drag start
+      // You can set the drag data here
+    },
+    endDrag() {
+      // Logic for drag end
+    },
+  },
+  created() {
+    // Call the loadCardImages method when the component is created
+    this.loadCardImages();
+  },
+};
+</script>
