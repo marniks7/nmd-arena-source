db48a2ab635cb66bc11c3e0584feac49a0e40bc4
marniks7 <9289172+marniks7@users.noreply.github.com>
2023-11-12 17:03:51 -0500

    New public path

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: process.env.NODE_ENV === 'production'
+      ? '/nmd-arena/'
+      : '/'
 })
